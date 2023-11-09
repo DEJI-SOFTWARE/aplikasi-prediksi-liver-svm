5c53bc9c58db7c04e1f0fa01c4b08a67c124756a
Feri Hermawan <ferihermawan007@gmail.com>
2023-11-09 10:14:22 +0700

    mengubah migrasi users

--- database/migrations/2023_11_09_022834_create_users_table.php
+++ database/migrations/2023_11_09_022834_create_users_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('users', function (Blueprint $table) {
             $table->id();
             $table->string('nama');
-            $table->string('username');
+            $table->string('email');
             $table->string('password');
             $table->string('image');
             $table->timestamps();
