8aa77f054ab00ddada553dcb9162b8a1222ce5b8
Feri Hermawan <ferihermawan007@gmail.com>
2023-11-12 21:06:46 +0700

    menambahkan fitur upload excel to database

--- app/Http/Controllers/DatasetController.php
+++ app/Http/Controllers/DatasetController.php
@@ -2,10 +2,24 @@
 
 namespace App\Http\Controllers;
 
+use App\Imports\DataTrainingImport;
 use Illuminate\Http\Request;
+use Maatwebsite\Excel\Facades\Excel;
 
 class DatasetController extends Controller
 {
+    public function StoreDataTraining(Request $request){
+        $this->validate($request,[
+            'file'=> 'required|mimes:csv,xls,xlsx'
+        ]);
+
+        $file = $request->file('file');
+        $namaFile = $file->getClientOriginalName();
+        $file->move('dataset', $namaFile);
+        Excel::import(new DataTrainingImport, public_path('/dataset/'.$namaFile));
+
+        return redirect()->back();
+    }
     public function UpdateDataTraining(){
 
     }
